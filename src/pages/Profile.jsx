9204493d7eacc4c0a3518e90a16bca93cb9eece2
leksansilva <69767375+leksansilva@users.noreply.github.com>
2022-07-24 23:41:14 -0300

    Correções

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,33 @@
+import { Header } from "../components/Profile/Header";
+import { Historic } from "../components/Profile/Historic";
+import { Others } from "../components/Profile/Others";
+import { Button } from "../components/Button";
+
+import logo from "../assets/logo/uneb.svg";
+import arrowLeft from "../assets/icons/arrowLeft.svg";
+
 export function Profile() {
-  return <div>Profile</div>;
+  return (
+    <>
+      <div className="my-5 flex gap justify-center">
+        <img src={logo} alt="Logo da Universidade" />
+      </div>
+      <div className="mx-3 m-auto bg-zinc-200 rounded-3xl flex-col justify-between">
+        <Header />
+        <div className="bg-blue-500 px-6 py-3">
+          <h2 className="text-white font-bold text-2xl">Histórico</h2>
+        </div>
+        <Historic />
+        <div className="bg-blue-500 px-6 py-3">
+          <h2 className="text-white font-bold text-2xl">Outros</h2>
+        </div>
+        <Others />
+      </div>
+      <div className="flex justify-center">
+        <Button className="my-10 rounded-lg" icon={arrowLeft}>
+          Voltar
+        </Button>
+      </div>
+    </>
+  );
 }
